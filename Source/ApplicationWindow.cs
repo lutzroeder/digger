fa133c4ef8671543884ca794e08e0cbdd7298c97
Lutz Roeder <lutzroeder@users.noreply.github.com>
2007-04-28 17:13:43 -0700

    Port for Windows Mobile and .NET Compact Framework

--- Source/ApplicationWindow.cs
+++ Source/ApplicationWindow.cs
@@ -1,46 +1,72 @@
 namespace Digger
 {
     using System;
-    using System.IO;
     using System.Collections;
     using System.Drawing;
-    using System.Drawing.Imaging;
+    using System.IO;
     using System.Windows.Forms;
 
-    public class ApplicationWindow : Form
+    internal class ApplicationWindow : Form
     { 
-        private int magnification = 1;
+        private byte[][] lastScreen = null;
         private Bitmap[] sprites = new Bitmap[34];
-        private FontLoader fontLoader;
+        private Bitmap[] characters = new Bitmap[63];
+        private Point origin = new Point(0, 0);
         private Timer timer;
-        private Engine engine;
         private int tick = 0;
+        private Engine engine;
         private int lives = 20;
         private bool[] keysRelease = new bool[4];
-        private bool[] keysDelay = new bool[4];  
-
-        public static void Main(string[] args)
+        private bool[] keysDelay = new bool[4];
+        private Size spriteSize;
+        private Size gridSize;
+        private Size clientSize;
+        private Image image;
+        
+        private static void Main(string[] args)
         {
             Application.Run(new ApplicationWindow());
         }
 
         public ApplicationWindow()
         {
-            this.Icon = new Icon(GetType().Assembly.GetManifestResourceStream("Application.ico"));
-            this.Text = ".NET Digger";
-            this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
-            this.MaximizeBox = false;    
+            this.Text = "Digger";
+            this.Icon = new Icon(this.GetType().Assembly.GetManifestResourceStream("Digger.Application.ico"));
 
-            this.ClientSize = MagnifyRectangle(0, 0, 320, 256).Size;
-            
-            this.fontLoader = new FontLoader(MagnifyBitmap(new Bitmap(GetType().Assembly.GetManifestResourceStream("Digger.Font.png"))), MagnifyRectangle(0, 0, 8, 8).Size);
+            int width = (this.ClientSize.Width > 320) ? 320 : this.ClientSize.Width;
+            int height = (this.ClientSize.Height > 232) ? 232 : this.ClientSize.Height;
+            this.clientSize = new Size(width, height);
+            this.image = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
 
-            Bitmap sprites = MagnifyBitmap(new Bitmap(GetType().Assembly.GetManifestResourceStream("Digger.Sprite.png")));
-            Rectangle cursor = MagnifyRectangle(0, 0, 16, 16);
-            for (int i = 0; i < 34; i++)
+            using (Graphics graphics = Graphics.FromImage(this.image))
             {
-                this.sprites[i] = sprites.Clone(cursor, PixelFormat.Format32bppArgb);
-                cursor.X += cursor.Width;
+                graphics.FillRectangle(new SolidBrush(Color.Black), 0, 0, this.image.Width, this.image.Height);
+            }
+
+            using (Stream stream = this.GetType().Assembly.GetManifestResourceStream("Digger.Sprites.bin"))
+            {
+                BinaryReader reader = new BinaryReader(stream);
+                for (int i = 0; i < 34; i++)
+                {
+                    int length = reader.ReadInt32();
+                    byte[] bytes = reader.ReadBytes(length);
+                    MemoryStream memoryStream = new MemoryStream(bytes);
+                    this.sprites[i] = new Bitmap(memoryStream);
+                    memoryStream.Close();
+                }
+            }
+
+            using (Stream stream = this.GetType().Assembly.GetManifestResourceStream("Digger.Font.bin"))
+            {
+                BinaryReader reader = new BinaryReader(stream);
+                for (int i = 0; i < 63; i++)
+                {
+                    int length = reader.ReadInt32();
+                    byte[] bytes = reader.ReadBytes(length);
+                    MemoryStream memoryStream = new MemoryStream(bytes);
+                    this.characters[i] = new Bitmap(memoryStream);
+                    memoryStream.Close();
+                }
             }
 
             using (Stream stream = GetType().Assembly.GetManifestResourceStream("Digger.Level.bin"))
@@ -50,61 +76,39 @@ namespace Digger
                 this.engine = new Engine(data);
             }
 
+            this.spriteSize = new Size(this.sprites[0].Width, this.sprites[0].Height);
+            this.gridSize = new Size();
+
+            this.gridSize.Width = this.clientSize.Width / this.spriteSize.Width;
+            if ((this.clientSize.Width % this.spriteSize.Width) != 0)
+            {
+                this.gridSize.Width++;
+            }
+
+            this.gridSize.Height = (this.clientSize.Height - 8) / this.spriteSize.Height;
+            if (((this.clientSize.Height - 8) % this.spriteSize.Height) != 0)
+            {
+                this.gridSize.Height++;
+            }
+
+            this.lives = 20;
+            this.engine.Level = 0;
             this.engine.Load();
+
             this.timer = new Timer();
             this.timer.Interval = 40;
             this.timer.Tick += new EventHandler(this.Timer_Tick);
-            this.timer.Start();
-        }
+            this.timer.Enabled = true;
 
-        protected override void OnKeyDown(KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Left) { this.engine.Keys[0] = true; keysDelay[0] = false; }
-            if (e.KeyCode == Keys.Right) { this.engine.Keys[1] = true; keysDelay[1] = false; }
-            if (e.KeyCode == Keys.Up) { this.engine.Keys[2] = true; keysDelay[2] = false; }
-            if (e.KeyCode == Keys.Down) { this.engine.Keys[3] = true; keysDelay[3] = false; }
-            if ((e.KeyCode == Keys.Escape) && (lives > 0)) { lives--; this.engine.Load(); }
-            if ((e.KeyCode == Keys.Home) && (this.engine.Level < 29)) { this.engine.Level++; this.engine.Load(); }
-        }
-
-        protected override void OnKeyUp(KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Left) 
-                if (keysDelay[0]) this.engine.Keys[0] = false; else keysRelease[0] = true;
-            if (e.KeyCode == Keys.Right)
-                if (keysDelay[1]) this.engine.Keys[1] = false; else keysRelease[1] = true;
-            if (e.KeyCode == Keys.Up)
-                if (keysDelay[2]) this.engine.Keys[2] = false; else keysRelease[2] = true;
-            if (e.KeyCode == Keys.Down)
-                if (keysDelay[3]) this.engine.Keys[3] = false; else keysRelease[3] = true;
-        }
-
-        private void Timer_Tick(object sender, EventArgs pe)
-        {
-            this.tick++;
-            this.engine.Tick1();
-
-            if ((this.tick % 2) == 0)
+            this.lastScreen = new byte[this.gridSize.Width][];
+            for (int i = 0; i < this.gridSize.Width; i++)
             {
-                if (this.keysRelease[0]) { this.engine.Keys[0] = this.keysRelease[0] = false; }
-                if (this.keysRelease[1]) { this.engine.Keys[1] = this.keysRelease[1] = false; }
-                if (this.keysRelease[2]) { this.engine.Keys[2] = this.keysRelease[2] = false; }
-                if (this.keysRelease[3]) { this.engine.Keys[3] = this.keysRelease[3] = false; }
-                this.engine.Tick4();
-            }
-
-            this.Invalidate(this.MagnifyRectangle(0, 8, 320, 16));
-
-            for (int y = 0; y < 14; y++)
-            {
-                for (int x = 0; x < 20; x++)
+                this.lastScreen[i] = new byte[this.gridSize.Height];
+                for (int j = 0; j < this.gridSize.Height; j++)
                 {
-                    Rectangle rectangle = MagnifyRectangle(x * 16, y * 16 + 32, 16, 16);
-                    this.Invalidate(rectangle);
+                    this.lastScreen[i][j] = 0xff;
                 }
             }
-
-            this.Update();
         }
 
         protected override void OnPaintBackground(PaintEventArgs e) 
@@ -113,91 +117,214 @@ namespace Digger
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            if ((e.ClipRectangle.Width != 0) && (e.ClipRectangle.Height != 0))
-            {
-                Graphics graphics = e.Graphics;
+            e.Graphics.DrawImage(this.image, e.ClipRectangle, e.ClipRectangle, GraphicsUnit.Pixel);
+        }
 
-                SolidBrush blueBrush = new SolidBrush(Color.FromArgb(0x04, 0x02, 0x8F));
-                graphics.FillRectangle(blueBrush, MagnifyRectangle(0, 0, 320, 8));
-                graphics.FillRectangle(blueBrush, MagnifyRectangle(0, 24, 320, 8));
-                SolidBrush cyanBrush = new SolidBrush(Color.FromArgb(0x2D, 0xE7, 0xC0));
-                graphics.FillRectangle(cyanBrush, MagnifyRectangle(0, 2, 320, 4));
-                graphics.FillRectangle(cyanBrush, MagnifyRectangle(0, 26, 320, 4));
-    
-                this.fontLoader.Reset();
-                this.fontLoader.Newline();
-                this.fontLoader.Write(graphics, "  CAVE:  " + (this.engine.Level + 1).ToString("D2") + " TIME:  " + this.engine.Time.ToString("D5") + " DIAMONDS:  " + this.engine.Diamonds.ToString("D2") + "  ");
-                this.fontLoader.Newline();
-                this.fontLoader.Write(graphics, "  LIVES: " + lives.ToString("D2") + " SCORE: " + this.engine.Score.ToString("D5") + " COLLECTED: " + this.engine.Collected.ToString("D2") + "  ");
-                this.fontLoader.Newline();
-    
-                for (int y = 0; y < 14; y++)
-                {
-                    for (int x = 0; x < 20; x++)
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    this.engine.Keys[0] = true; 
+                    keysDelay[0] = false; 
+                    break;
+
+                case Keys.Right:
+                    this.engine.Keys[1] = true; 
+                    keysDelay[1] = false; 
+                    break;
+
+                case Keys.Up:
+                    this.engine.Keys[2] = true; 
+                    keysDelay[2] = false; 
+                    break;
+
+                case Keys.Down:
+                    this.engine.Keys[3] = true; 
+                    keysDelay[3] = false; 
+                    break;
+
+                case Keys.F8:
+                    if (this.lives > 0)
                     {
-                        Rectangle rectangle = this.MagnifyRectangle(x * 16, y * 16 + 32, 16, 16);
-                        if (e.ClipRectangle.IntersectsWith(rectangle)) 
-                            graphics.DrawImageUnscaled(sprites[this.engine.GetSpriteImageIndex(x, y)], rectangle.X, rectangle.Y);
+                        this.lives--;
+                        this.engine.Load();
+                    }
+                    else
+                    {
+                        // Restart game
+                        this.lives = 20;
+                        this.engine.Level = 0;
+                        this.engine.Load();
+                    }
+                    break;
+
+                case Keys.F9:
+                    ApplicationWindow applicationWindow = (ApplicationWindow) this.TopLevelControl;
+                    applicationWindow.Close();
+                    break;
+
+                case Keys.D0:
+                    if (this.engine.Level < 29)
+                    {
+                        this.engine.Level++;
+                        this.engine.Load();
+                    }
+                    break;
+            }
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) 
+            {
+                if (keysDelay[0]) 
+                {
+                    this.engine.Keys[0] = false; 
+                }
+                else 
+                {
+                    keysRelease[0] = true;
+                }
+            }
+
+            if (e.KeyCode == Keys.Right) 
+            {
+                if (keysDelay[1]) 
+                {
+                    this.engine.Keys[1] = false; 
+                }
+                else 
+                {
+                    keysRelease[1] = true;
+                }
+            }
+
+            if (e.KeyCode == Keys.Up) 
+            {
+                if (keysDelay[2]) 
+                {
+                    this.engine.Keys[2] = false; 
+                }
+                else 
+                {
+                    keysRelease[2] = true;
+                }
+            }
+
+            if (e.KeyCode == Keys.Down) 
+            {
+                if (keysDelay[3]) 
+                {
+                    this.engine.Keys[3] = false; 
+                }
+                else 
+                {
+                    keysRelease[3] = true;
+                }
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            this.tick++;
+            this.engine.Tick1();
+
+            if ((this.tick % 2) == 0)
+            {
+                if (this.keysRelease[0]) 
+                {
+                    this.engine.Keys[0] = this.keysRelease[0] = false; 
+                }
+                
+                if (this.keysRelease[1]) 
+                {
+                    this.engine.Keys[1] = this.keysRelease[1] = false; 
+                }
+                
+                if (this.keysRelease[2]) 
+                {
+                    this.engine.Keys[2] = this.keysRelease[2] = false; 
+                }
+                
+                if (this.keysRelease[3]) 
+                {
+                    this.engine.Keys[3] = this.keysRelease[3] = false; 
+                }
+
+                this.engine.Tick4();
+            }
+
+            int[] position = this.engine.GetCurrentPosition();
+
+            if (this.origin.X < (20 - this.gridSize.Width))
+            {
+                if (((this.origin.X + this.gridSize.Width) - position[0]) < 6)
+                {
+                    this.origin.X++;
+                }
+            }
+
+            if (this.origin.Y < (14 - this.gridSize.Height))
+            {
+                if (((this.origin.Y + this.gridSize.Height) - position[1]) < 5)
+                {
+                    this.origin.Y++;
+                }
+            }
+
+            if ((this.origin.X > 0) && ((position[0] - this.origin.X) < 6))
+            {
+                this.origin.X--;
+            }
+
+            if ((this.origin.Y > 0) && ((position[1] - this.origin.Y) < 5))
+            {
+                this.origin.Y--;
+            }
+
+            using (Graphics graphics = Graphics.FromImage(this.image))
+            {
+                string text = " [" + (this.engine.Level + 1).ToString("D2") + " \\" + this.lives.ToString("D2") + " ]" + this.engine.Collected.ToString("D2") + "/" + this.engine.Diamonds.ToString("D2") + " ^" + this.engine.Time.ToString("D4") + " ";
+                Point p = new Point(0, 0);
+                for (int i = 0; i < text.Length; i++)
+                {
+                    graphics.DrawImage(this.characters[(byte)(text[i] - 32)], p.X, p.Y);
+                    p.X += 8;
+                }
+
+                this.Invalidate(new Rectangle(0, 0, 180, 8));
+
+                for (int x = 0; x < this.gridSize.Width; x++)
+                {
+                    for (int y = 0; y < this.gridSize.Height; y++)
+                    {
+                        Rectangle rectangle = new Rectangle(x * 16, (y * 16) + 8, 16, 16);
+
+                        if (rectangle.Bottom > this.clientSize.Height)
+                        {
+                            rectangle.Height = this.clientSize.Height - rectangle.Top;
+                        }
+
+                        if (rectangle.Right > this.clientSize.Width)
+                        {
+                            rectangle.Width = this.clientSize.Width - rectangle.Left;
+                        }
+
+                        byte imageIndex = (byte)this.engine.GetSpriteImageIndex(this.origin.X + x, this.origin.Y + y);
+                        if (imageIndex != this.lastScreen[x][y])
+                        {
+                            Rectangle srcRect = new Rectangle(0, 0, rectangle.Width, rectangle.Height);
+                            graphics.DrawImage(this.sprites[imageIndex], rectangle, srcRect, GraphicsUnit.Pixel);
+                            this.lastScreen[x][y] = imageIndex;
+
+                            this.Invalidate(rectangle);
+                        }
                     }
                 }
             }
-        }
 
-        private Rectangle MagnifyRectangle(int x, int y, int dx, int dy)
-        {
-            return new Rectangle(x * magnification, y * magnification, dx * magnification, dy * magnification);
-        }
-
-        private Bitmap MagnifyBitmap(Bitmap source)
-        {
-            Bitmap target = new Bitmap(source.Width * magnification, source.Height * magnification);
-            for (int x = 0; x < source.Width; x++)
-                for (int y = 0; y < source.Height; y++)
-                    for (int ix = 0; ix < magnification; ix++)
-                        for (int iy = 0; iy < magnification; iy++)
-                            target.SetPixel(x * magnification + ix, y * magnification + iy, source.GetPixel(x, y));
-            return target;
-        }
-
-        private class FontLoader
-        {
-            private Point position = new Point(0, 0);
-            private Size size;
-            private Bitmap[] characters = new Bitmap[59];
-
-            public FontLoader(Bitmap bitmap, Size size)
-            {
-                this.size = size;
-                Rectangle cursor = new Rectangle(0, 0, size.Width, size.Height);
-                for (int i = 0; i < 59; i++)
-                {
-                    this.characters[i] = bitmap.Clone(cursor, PixelFormat.Format24bppRgb);
-                    this.characters[i].MakeTransparent(Color.FromArgb(0, 0, 0));
-                    cursor.Y += cursor.Height;
-                }
-            }
-
-            public void Reset()
-            {
-                this.position = new Point(0, 0);
-            }
-
-            public void Write(Graphics graphics, string text)
-            {
-                SolidBrush redBrush = new SolidBrush(Color.FromArgb(0x92, 0x02, 0x05));
-                for (int i = 0; i < text.Length; i++)
-                {
-                    graphics.FillRectangle(redBrush, new Rectangle(this.position.X, this.position.Y, this.size.Width, this.size.Height));
-                    graphics.DrawImageUnscaled(this.characters[(byte)(text[i] - 32)], this.position.X, this.position.Y);
-                    this.position.X += size.Width;
-                }
-            }
-
-            public void Newline()
-            {
-                this.position.X = 0;
-                this.position.Y += size.Height;
-            }
+            this.Update();
         }
     }
 }
--- Source/Build.bat
+++ Source/Build.bat
@@ -1,16 +1,17 @@
-@echo off
+@echo off 
 
-set FrameworkVersion=1.0.3705
-if exist %SystemRoot%\Microsoft.NET\Framework\v%FrameworkVersion%\csc.exe goto :Start
-set FrameworkVersion=1.1.4322
-if exist %SystemRoot%\Microsoft.NET\Framework\v%FrameworkVersion%\csc.exe goto :Start
-set FrameworkVersion=2.0.50727
-if exist %SystemRoot%\Microsoft.NET\Framework\v%FrameworkVersion%\csc.exe goto :Start
-:Start
+rd /S /Q bin
+rd /S /Q obj
 
-if exist bin rd /S /Q bin
-if exist obj rd /S /Q obj
-if exist ..\Build rd /S /Q ..\Build
-md ..\Build
+set FrameworkPath=%ProgramFiles%\Microsoft Visual Studio 8\SmartDevices\SDK\CompactFramework\2.0\v1.0\WindowsCE\
 
-%SystemRoot%\Microsoft.net\Framework\v%FrameworkVersion%\csc.exe /target:winexe /out:..\Build\Digger.exe ApplicationWindow.cs Engine.cs /win32icon:Application.ico /resource:Application.ico /resource:Font.png,Digger.Font.png /resource:Level.bin,Digger.Level.bin /resource:Sprite.png,Digger.Sprite.png %1
+set ToolsPath=%ProgramFiles%\Microsoft Visual Studio 8\SmartDevices\SDK\SDKTools\
+
+%SystemRoot%\Microsoft.net\Framework\v1.1.4322\csc.exe /noconfig /nostdlib /target:winexe /out:..\Build\Digger.exe ApplicationWindow.cs Engine.cs /win32icon:Application.ico /reference:"%FrameworkPath%\mscorlib.dll" /reference:"%FrameworkPath%\System.dll" /reference:"%FrameworkPath%\System.Drawing.dll" /reference:"%FrameworkPath%\System.Windows.Forms.dll" /resource:Application.ico,Digger.Application.ico /resource:Font.bin,Digger.Font.bin /resource:Level.bin,Digger.Level.bin /resource:Sprites.bin,Digger.Sprites.bin
+
+pushd ..\Build
+copy ..\Source\Digger.inf
+"%ToolsPath%\cabwiz.exe" Digger.inf
+del Digger.inf
+ren Digger.CAB Digger.cab
+popd
